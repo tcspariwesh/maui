ceb18e48d74ee7a148e9b647fa903b987dbf07bb
Pariwesh Gupta (C) <pariwesh@alchemysolutions.net>
2023-10-09 22:51:58 +0530

    first commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace MauiApp2
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+           // display.BindingContext = slider;
+         //   display.SetBinding(Label.TextProperty, "Value");
+        }
+        private void handleCheckbox()
+        {
+
+        }
+
+       /* private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time {e}";
+            else
+                CounterBtn.Text = $"Clicked {count} times {e}";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }*/
+    }
+}
